545b942125d60026b19e8d9561f3819a32be7f3b
Chris-GW <Chris-GW@users.noreply.github.com>
2018-09-03 10:21:55 +0200

    finish 6 kyu kata "Srot the inner ctonnet in dsnnieedcg oredr"

---
+++ src/test/java/com/codewars/chrisgw/algorithms/kyu_6/SrotTheInnerCtonnetInDsnnieedcgOredrTest.java
@@ -0,0 +1,20 @@
+package com.codewars.chrisgw.algorithms.kyu_6;
+
+import org.junit.Test;
+
+import static com.codewars.chrisgw.algorithms.kyu_6.SrotTheInnerCtonnetInDsnnieedcgOredr.sortTheInnerContent;
+import static org.junit.Assert.*;
+
+
+public class SrotTheInnerCtonnetInDsnnieedcgOredrTest {
+
+
+    @Test
+    public void exampleTests() {
+        assertEquals("srot the inner ctonnet in dsnnieedcg oredr",
+                sortTheInnerContent("sort the inner content in descending order"));
+        assertEquals("wiat for me", sortTheInnerContent("wait for me"));
+        assertEquals("tihs ktaa is esay", sortTheInnerContent("this kata is easy"));
+    }
+
+}
