31e5f712b4f5575925185805d52edee9cf3b2438
Chris-GW <Chris-GW@users.noreply.github.com>
2018-09-03 10:23:11 +0200

    start 6kyu "length-of-missing-array"

---
+++ src/main/java/com/codewars/chrisgw/algorithms/kyu_6/LengthOfMissingArray.java
@@ -0,0 +1,27 @@
+package com.codewars.chrisgw.algorithms.kyu_6;
+
+/**
+ * You get an array of arrays.<br>
+ * If you sort the arrays by their length, you will see, that their length-values are consecutive.<br>
+ * But one array is missing!<br>
+ * <br><br>
+ * You have to write a method, that return the length of the missing array.<br>
+ * ```
+ * Example:
+ * [[1, 2], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]] --> 3
+ * ```
+ * <br>
+ * 
+ * If the array of arrays is null/nil or empty, the method should return 0.<br>
+ * 
+ * When an array in the array is null or empty, the method should return 0 too!<br>
+ * There will always be a missing element and its length will be always between the given arrays.
+ * <br><br>
+ * Have fun coding it and please don't forget to vote and rank this kata! :-)<br> 
+ * <br>
+ * I have created other katas. Have a look if you like coding and challenges.
+ *
+ */
+public class LengthOfMissingArray {
+
+}
---
+++ src/test/java/com/codewars/chrisgw/algorithms/kyu_6/LengthOfMissingArrayTest.java
@@ -0,0 +1,5 @@
+package com.codewars.chrisgw.algorithms.kyu_6;
+
+public class LengthOfMissingArrayTest {
+
+}
