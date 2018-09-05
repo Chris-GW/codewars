1c9be24532ac61b6518f58cfcf8007c766f3f378
Chris-GW <Chris-GW@users.noreply.github.com>
2018-09-05 13:08:49 +0200

    start 5 kyu best-travel

---
+++ src/test/java/com/codewars/chrisgw/reference/kyu_5/BestTravelTest.java
@@ -0,0 +1,30 @@
+package com.codewars.chrisgw.reference.kyu_5;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.codewars.chrisgw.reference.kyu_5.BestTravel.chooseBestSum;
+import static org.junit.Assert.*;
+
+
+public class BestTravelTest {
+
+
+    @Test
+    public void BasicTests1() {
+        System.out.println("****** Basic Tests small numbers******");
+        List<Integer> ts = new ArrayList<>(Arrays.asList(50, 55, 56, 57, 58));
+        int n = chooseBestSum(163, 3, ts);
+        assertEquals(163, n);
+        ts = new ArrayList<>(Arrays.asList(50));
+        Integer m = chooseBestSum(163, 3, ts);
+        assertEquals(null, m);
+        ts = new ArrayList<>(Arrays.asList(91, 74, 73, 85, 73, 81, 87));
+        n = chooseBestSum(230, 3, ts);
+        assertEquals(228, n);
+    }
+
+}
