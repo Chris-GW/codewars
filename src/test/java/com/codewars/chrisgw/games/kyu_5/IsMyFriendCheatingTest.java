3891864d95a523fe47c66a491f2ff11118e2e348
Chris-GW <Chris-GW@users.noreply.github.com>
2018-09-06 14:52:27 +0200

    start 5 kyu is-my-friend-cheating

---
+++ src/test/java/com/codewars/chrisgw/games/kyu_5/IsMyFriendCheatingTest.java
@@ -0,0 +1,29 @@
+package com.codewars.chrisgw.games.kyu_5;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class IsMyFriendCheatingTest {
+
+    @Test
+    public void test12() {
+        List<long[]> res = new ArrayList<long[]>();
+        res.add(new long[] {15, 21});
+        res.add(new long[] {21, 15});
+        List<long[]> a = RemovedNumbers.removNb(26);
+        assertArrayEquals(res.get(0), a.get(0));
+        assertArrayEquals(res.get(1), a.get(1));
+    }
+    @Test
+    public void test14() {
+        List<long[]> res = new ArrayList<long[]>();
+        List<long[]> a = IsMyFriendCheating.removNb(100);
+        assertTrue(res.size() == a.size());
+    }
+
+}
