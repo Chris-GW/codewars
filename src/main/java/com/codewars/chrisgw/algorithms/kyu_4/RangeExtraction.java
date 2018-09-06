c04d8b2f2dbebf6e9f42c67c20d01f2dc0ffcdfa
Chris-GW <Chris-GW@users.noreply.github.com>
2018-09-06 09:58:59 +0200

    start 4 kyu range-extraction

---
+++ src/main/java/com/codewars/chrisgw/algorithms/kyu_4/RangeExtraction.java
@@ -0,0 +1,21 @@
+package com.codewars.chrisgw.algorithms.kyu_4;
+
+/**
+ * A format for expressing an ordered list of integers is to use a comma separated list of either
+ * <p>
+ * * individual integers
+ * * or a range of integers denoted by the starting integer separated from the end integer in the range by a dash, '-'. The range includes all integers in the interval including both endpoints.  It is not considered a range unless it spans at least 3 numbers. For example ("12, 13, 15-17")
+ * <p>
+ * Complete the solution  so that it takes a list of integers in increasing order and returns a correctly formatted string in the range format.
+ * <p>
+ * **Example:**
+ * <p>
+ * *Courtesy of rosettacode.org*
+ */
+public class RangeExtraction {
+
+    public static String rangeExtraction(int[] arr) {
+        return "";
+    }
+
+}
