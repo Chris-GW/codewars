a085bc2692a60588a10a2807ac5b3329f6e9696f
Chris-GW <Chris-GW@users.noreply.github.com>
2018-09-06 15:19:08 +0200

    start 4 kyu how-many-numbers-iii

---
+++ src/main/java/com/codewars/chrisgw/reference/kyu_4/HowManyNumbersIii.java
@@ -0,0 +1,40 @@
+package com.codewars.chrisgw.reference.kyu_4;
+
+import java.util.List;
+
+
+/**
+ * We want to generate all the numbers of three digits that:
+ * <p>
+ * - the value of adding their corresponding ones(digits) is equal to 10.
+ * <p>
+ * - their digits are in increasing order (the numbers may have two or more equal contiguous digits)
+ * <p>
+ * The numbers that fulfill the two above constraints are: ```118, 127, 136, 145, 226, 235, 244, 334```
+ * <p>
+ * Make a function that receives two arguments:
+ * <p>
+ * - the sum of digits value
+ * <p>
+ * - the amount of desired digits for the numbers
+ * <p>
+ * The function should output an array with three values:
+ * [1,2,3]
+ * <p>
+ * 1 - the total amount of all these possible numbers
+ * <p>
+ * 2 - the minimum number
+ * <p>
+ * 3 - the maximum numberwith
+ * <p>
+ * The example given above should be:
+ */
+public class HowManyNumbersIii {
+
+
+    public static List<Long> findAll(final int sumDigits, final int numDigits) {
+        // Your code here!!
+        return null;
+    }
+
+}
---
+++ src/test/java/com/codewars/chrisgw/reference/kyu_4/HowManyNumbersIiiTest.java
@@ -0,0 +1,21 @@
+package com.codewars.chrisgw.reference.kyu_4;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+
+public class HowManyNumbersIiiTest {
+
+    @Test
+    public void exampleTests() {
+        assertEquals(Arrays.asList(8L, 118L, 334L), HowManyNumbersIii.findAll(10, 3));
+        assertEquals(Arrays.asList(1L, 999L, 999L), HowManyNumbersIii.findAll(27, 3));
+        assertEquals(new ArrayList<Long>(), HowManyNumbersIii.findAll(84, 4));
+        assertEquals(Arrays.asList(123L, 116999L, 566666L), HowManyNumbersIii.findAll(35, 6));
+    }
+
+}
