0baa49b40e9366d157b8ad51924ed8f57afd31b5
Chris-GW <Chris-GW@users.noreply.github.com>
2018-09-07 10:42:52 +0200

    finish 4 kyu the-observed-pin

--- src/main/java/com/codewars/chrisgw/algorithms/kyu_4/TheObservedPin.java
+++ src/main/java/com/codewars/chrisgw/algorithms/kyu_4/TheObservedPin.java
@@ -1,6 +1,7 @@
 package com.codewars.chrisgw.algorithms.kyu_4;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.IntStream;
 
 
 /**
@@ -24,15 +25,67 @@ import java.util.List;
  * <p>
  * \* possible in sense of: the observed PIN itself and all variations considering the adjacent digits
  * <p>
- * Can you help us to find all those variations? It would be nice to have a function, that returns an array (or a list in Java and C#)  of all variations for an observed PIN with a length of 1 to 8 digits. We could name the function `getPINs` (`get_pins` in python, `GetPINs` in C#). But please note that all PINs, the observed one and also the results, must be strings, because of potentially leading '0's. We already prepared some test cases for you.
+ * Can you help us to find all those variations? It would be nice to have a function, that returns an array (or a list in Java and C#)  of all variations for an observed PIN with a length of 1 to 8 digits. We could name the function `getPINsDethFirstSearch` (`get_pins` in python, `GetPINs` in C#). But please note that all PINs, the observed one and also the results, must be strings, because of potentially leading '0's. We already prepared some test cases for you.
  * <p>
  * Detective, we count on you!
  */
 public class TheObservedPin {
 
+    private static int[][] keypad = new int[][] { //
+            new int[] { 1, 2, 3 }, //
+            new int[] { 4, 5, 6 }, //
+            new int[] { 7, 8, 9 }, //
+            new int[] { -1, 0, -1 }, //
+    };
 
     public static List<String> getPINs(String observed) {
-        return null;
+        return getPINsDethFirstSearch(new StringBuilder(observed), 0);
+    }
+
+    private static List<String> getPINsDethFirstSearch(StringBuilder observedPin, int numberIndex) {
+        if (numberIndex >= observedPin.length()) {
+            return Collections.singletonList(observedPin.toString());
+        }
+        List<String> possiblePINs = new LinkedList<>();
+        int observedNumber = observedPin.charAt(numberIndex) - '0';
+        possibleAdjecentNumbers(observedNumber).forEach(possibleNumber -> {
+            observedPin.setCharAt(numberIndex, Character.forDigit(possibleNumber, 10));
+            possiblePINs.addAll(getPINsDethFirstSearch(observedPin, numberIndex + 1));
+            observedPin.setCharAt(numberIndex, Character.forDigit(observedNumber, 10));
+        });
+        return possiblePINs;
+    }
+
+    private static Set<Integer> possibleAdjecentNumbers(int number) {
+        for (int row = 0; row < keypad.length; row++) {
+            for (int column = 0; column < keypad[row].length; column++) {
+                if (keypad[row][column] == number) {
+                    return possibleAdjecentNumbers(row, column);
+                }
+            }
+        }
+        return Collections.emptySet();
+    }
+
+    private static Set<Integer> possibleAdjecentNumbers(int row, int column) {
+        Set<Integer> possibleAdjecentNumbers = new HashSet<>(4);
+        IntStream.rangeClosed(-1, 1)
+                .map(rowOffset -> row + rowOffset)
+                .filter(rowIndex -> isValidKeypadPosition(rowIndex, column))
+                .map(rowIndex -> keypad[rowIndex][column])
+                .forEach(possibleAdjecentNumbers::add);
+        IntStream.rangeClosed(-1, 1)
+                .map(columnOffset -> column + columnOffset)
+                .filter(columnIndex -> isValidKeypadPosition(row, columnIndex))
+                .map(columnIndex -> keypad[row][columnIndex])
+                .forEach(possibleAdjecentNumbers::add);
+        return possibleAdjecentNumbers;
+    }
+
+    private static boolean isValidKeypadPosition(int rowIndex, int columnIndex) {
+        boolean isValidKeypadRow = 0 <= rowIndex && rowIndex < keypad.length;
+        return isValidKeypadRow && 0 <= columnIndex && columnIndex < keypad[rowIndex].length
+                && keypad[rowIndex][columnIndex] >= 0;
     }
 
 }
