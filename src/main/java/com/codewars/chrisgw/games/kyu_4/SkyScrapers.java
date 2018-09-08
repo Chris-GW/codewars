e8dc2cb0bd911349f88be42885cbbb3455ebe5e5
Chris-GW <Chris-GW@users.noreply.github.com>
2018-09-08 19:42:50 +0200

    edit 4 kyu 4-by4-skyscrapers

--- src/main/java/com/codewars/chrisgw/games/kyu_4/SkyScrapers.java
+++ src/main/java/com/codewars/chrisgw/games/kyu_4/SkyScrapers.java
@@ -1,5 +1,12 @@
 package com.codewars.chrisgw.games.kyu_4;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+
 /**
  * <style type="text/css">
  * table {
@@ -231,8 +238,205 @@ package com.codewars.chrisgw.games.kyu_4;
 public class SkyScrapers {
 
 
-    public static int[][] solvePuzzle(int[] clues) {
-        return new int[4][4];
+    private SkyScraper[][] skyScraperGrid;
+    private final int[] clues;
+
+
+    public SkyScrapers(SkyScraper[][] skyScraperGrid, int[] clues) {
+        this.skyScraperGrid = skyScraperGrid;
+        this.clues = clues;
     }
 
+    public static int[][] solvePuzzle(int[] clues) {
+        int skyScraperGridSize = clues.length / 4;
+        SkyScraper[][] skyScraperGrid = new SkyScraper[skyScraperGridSize][skyScraperGridSize];
+        for (int row = 0; row < skyScraperGridSize; row++) {
+            for (int column = 0; column < skyScraperGridSize; column++) {
+                skyScraperGrid[row][column] = new SkyScraper(row, column, skyScraperGridSize);
+                char letter = (char) ('A' + column);
+                skyScraperGrid[row][column].name = letter + String.valueOf(row);
+            }
+        }
+        return new SkyScrapers(skyScraperGrid, clues).solvePuzzle();
+    }
+
+
+    public int[][] solvePuzzle() {
+        for (int clueIndex = 0; clueIndex < clues.length; clueIndex++) {
+            int heightClue = clues[clueIndex];
+            List<SkyScraper> skyScrapers = skyScraperForClueIndex(clueIndex);
+            System.out.println(clueIndex + " => " + heightClue + ": " + skyScrapers.stream()
+                    .map(skyScraper -> skyScraper.name)
+                    .collect(Collectors.joining(", ")));
+            System.out.println(toString());
+            if (heightClue == 0) {
+                continue;
+            }
+
+            System.out.println("For clueIndex: " + clueIndex + " = " + heightClue);
+            Set<int[]> possibleHeightCombinations = possibleHeightCombinationsForGivenClue(skyScrapers, heightClue, 0);
+
+            skyScrapers.forEach(skyScraper -> skyScraper.possibleSkyScraperHeights.clear());
+            for (int[] possibleHeightCombination : possibleHeightCombinations) {
+                for (int i = 0; i < possibleHeightCombination.length; i++) {
+                    SkyScraper skyScraper = skyScrapers.get(i);
+                    skyScraper.addPossibleHeight(possibleHeightCombination[i]);
+                }
+            }
+        }
+        return toSkyScraperGrid2D();
+    }
+
+    private Set<int[]> possibleHeightCombinationsForGivenClue(List<SkyScraper> skyScrapers, int heightClue, int index) {
+        int seenSkyScrapers = getSeenSkyScrapers(skyScrapers);
+        if (index >= skyScrapers.size() && seenSkyScrapers == heightClue) {
+            Set<int[]> possibleHeight = new HashSet<>();
+            possibleHeight.add(skyScrapers.stream().mapToInt(value -> value.height).toArray());
+            System.out.println(skyScrapers + " => " + seenSkyScrapers);
+            return possibleHeight;
+        } else if (index >= skyScrapers.size()) {
+            return new HashSet<>(0);
+        }
+
+        Set<int[]> possibleHeightCombinations = new HashSet<>();
+
+        SkyScraper skyScraper = skyScrapers.get(index);
+        Set<Integer> possibleSkyScraperHeights = new HashSet<>(skyScraper.possibleSkyScraperHeights);
+        possibleSkyScraperHeights.removeAll(getUsedHeight(skyScrapers));
+        for (int height : possibleSkyScraperHeights) {
+            skyScraper.height = height;
+            possibleHeightCombinations.addAll(
+                    possibleHeightCombinationsForGivenClue(skyScrapers, heightClue, index + 1));
+            skyScraper.height = 0;
+        }
+
+        return possibleHeightCombinations;
+    }
+
+    private Set<Integer> getUsedHeight(List<SkyScraper> skyScrapers) {
+        return skyScrapers.stream().map(skyScraper -> skyScraper.height).collect(Collectors.toSet());
+    }
+
+
+    private int[][] toSkyScraperGrid2D() {
+        int[][] skyScraperGrid2D = new int[skyScraperGrid.length][];
+        for (int row = 0; row < skyScraperGrid.length; row++) {
+            SkyScraper[] skyScraperRow = skyScraperGrid[row];
+            skyScraperGrid2D[row] = new int[skyScraperRow.length];
+            for (int column = 0; column < skyScraperRow.length; column++) {
+                skyScraperGrid2D[row][column] = skyScraperRow[column].getHeight();
+            }
+            System.out.println(Arrays.toString(skyScraperGrid2D[row]));
+        }
+        return skyScraperGrid2D;
+    }
+
+
+    public List<SkyScraper> skyScraperForClueIndex(int clueIndex) {
+        int side = clueIndex / skyScraperGrid.length;
+        List<SkyScraper> skyScrapers = new ArrayList<>(skyScraperGrid.length);
+
+        for (int i = 0; i < skyScraperGrid.length; i++) {
+            if (side == 0) { // top
+                int column = clueIndex % skyScraperGrid.length;
+                skyScrapers.add(skyScraperGrid[i][column]);
+            } else if (side == 2) { // bottom
+                int column = skyScraperGrid.length - 1 - clueIndex % skyScraperGrid.length;
+                skyScrapers.add(skyScraperGrid[skyScraperGrid.length - 1 - i][column]);
+
+            } else if (side == 1) { // right
+                int row = clueIndex % skyScraperGrid.length;
+                skyScrapers.add(skyScraperGrid[row][skyScraperGrid.length - 1 - i]);
+            } else if (side == 3) { // left
+                int row = skyScraperGrid.length - 1 - clueIndex % skyScraperGrid.length;
+                skyScrapers.add(skyScraperGrid[row][i]);
+
+            }
+        }
+        skyScrapers.forEach(skyScraper -> skyScraper.height = 0);
+        return skyScrapers;
+    }
+
+
+    private static Set<Integer> allPossibleHeights(int maxHeight) {
+        return IntStream.rangeClosed(1, maxHeight).boxed().collect(Collectors.toSet());
+    }
+
+
+    private static int getSeenSkyScrapers(List<SkyScraper> skyScrapers) {
+        int skyScraperCount = 0;
+        int currentHight = 0;
+        for (SkyScraper skyScraper : skyScrapers) {
+            if (currentHight < skyScraper.height) {
+                skyScraperCount++;
+                currentHight = skyScraper.height;
+            }
+        }
+        return skyScraperCount;
+    }
+
+
+    public static class SkyScraper {
+
+        int row;
+        int column;
+        String name = "A0";
+
+        int height = 0;
+        Set<Integer> possibleSkyScraperHeights;
+
+        public SkyScraper(int row, int column, int maxHeight) {
+            this.row = row;
+            this.column = column;
+            this.possibleSkyScraperHeights = allPossibleHeights(maxHeight);
+        }
+
+        public int getHeight() {
+            return possibleSkyScraperHeights.stream().findAny().orElse(0);
+        }
+
+        public boolean addPossibleHeight(int possibleHeight) {
+            if (possibleHeight > 0) {
+                return possibleSkyScraperHeights.add(possibleHeight);
+            }
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(height);
+        }
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(StringUtils.repeat("-", 31)).append("\n");
+        for (int row = 0; row < skyScraperGrid.length; row++) {
+            for (int i = 0; i <= 1; i++) {
+                if (i == 1) {
+                    sb.append("\n");
+                }
+                for (int column = 0; column < skyScraperGrid.length; column++) {
+                    SkyScraper skyScraper = skyScraperGrid[row][column];
+                    if (i == 0) {
+                        sb.append(StringUtils.leftPad(skyScraper.possibleSkyScraperHeights.stream()
+                                .filter(height -> height <= 2)
+                                .map(String::valueOf)
+                                .collect(Collectors.joining(", ")), 5));
+                    } else {
+                        sb.append(StringUtils.leftPad(skyScraper.possibleSkyScraperHeights.stream()
+                                .filter(height -> height >= 3)
+                                .map(String::valueOf)
+                                .collect(Collectors.joining(", ")), 5));
+                    }
+                    sb.append(" | ");
+                }
+            }
+            sb.append("\n").append(StringUtils.repeat("-", 31)).append("\n");
+        }
+
+        return sb.toString();
+    }
 }
