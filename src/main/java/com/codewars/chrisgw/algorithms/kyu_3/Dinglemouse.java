d862ac9545b493e7b0e3e8215b6dce35da6456f6
Chris-GW <Chris-GW@users.noreply.github.com>
2018-09-08 15:05:18 +0200

    refactor 3 kyu the-lift

--- src/main/java/com/codewars/chrisgw/algorithms/kyu_3/Dinglemouse.java
+++ src/main/java/com/codewars/chrisgw/algorithms/kyu_3/Dinglemouse.java
@@ -193,6 +193,199 @@ public class Dinglemouse {
         return sb.toString();
     }
 
+
+    public class Lift {
+
+        private Floor currentFloor;
+
+        private boolean isGoingUpwards = true;
+        private List<Floor> stoppedFloors;
+
+        private int personCapazity;
+        private List<Person> loadedPersons;
+
+
+        public Lift(Floor startingFloor, int personCapazity) {
+            this.currentFloor = startingFloor;
+            this.personCapazity = personCapazity;
+            this.loadedPersons = new LinkedList<>();
+            this.stoppedFloors = new LinkedList<>();
+            this.stoppedFloors.add(startingFloor);
+        }
+
+
+        /**
+         * Operates this lift, which will transport all {@link Person}s to their destination {@link Floor}s
+         * and return in the end back to the first {@link Floor}.
+         */
+        public void operateLift() {
+            boolean hasMoved = false;
+            System.out.println("                         +-a-+");
+            System.out.println(Dinglemouse.this.toString());
+            for (char step = 'b'; true; step++) {
+                unloadPersons();
+                loadPersons();
+
+                Optional<Floor> nextFloor = firstFloorInTravelDirection();
+                if (!nextFloor.isPresent()) {
+                    nextFloor = lastFloorInDirectionWherePersonWantToEnter();
+                    reverseLiftDirection();
+                }
+
+                if (nextFloor.isPresent()) {
+                    moveLift(nextFloor.get());
+                    hasMoved = true;
+                } else if (!hasMoved) {
+                    // 5. repeat 1 - 4 till this lift hasn't moved any more (no more persons want to ride)
+                    break;
+                } else {
+                    hasMoved = false;
+                }
+                System.out.println("                         +-" + step + "-+");
+                System.out.println(Dinglemouse.this.toString());
+            }
+
+            moveLift(buildingFloors[0]);
+            System.out.println("finish with stoppedFloors: " + stoppedFloors);
+        }
+
+
+        /**
+         * 1. unloads all {@link Person}s from the {@link Lift#loadedPersons},
+         * which have as destination the {@link #currentFloor}.
+         */
+        private void unloadPersons() {
+            for (int i = 0; i < loadedPersons.size(); i++) {
+                Person person = loadedPersons.get(i);
+                if (person.destinationFloor == currentFloor.floorLevel) {
+                    loadedPersons.remove(i--);
+                    currentFloor.personQueue.addLast(person);
+                }
+            }
+            System.out.println("unloadPersons to Floor " + currentFloor + ": " + loadedPersons);
+        }
+
+        /**
+         * 2. loads all {@link Person}s from {@link #currentFloor}, which want to ride in lifts direction.
+         * Following {@link Floor#personQueue} in order and given {@link #personCapazity}.
+         */
+        private void loadPersons() {
+            List<Person> personsEntering;
+            if (isGoingUpwards) {
+                personsEntering = currentFloor.personsGoingUpwards() //
+                        .limit(getFreePersonCapazity()) //
+                        .collect(Collectors.toList());
+            } else {
+                personsEntering = currentFloor.personsGoingDownwards() //
+                        .limit(getFreePersonCapazity()) //
+                        .collect(Collectors.toList());
+            }
+            personsEntering.forEach(person -> {
+                currentFloor.personQueue.remove(person);
+                loadedPersons.add(person);
+            });
+            System.out.println("loadPersons from Floor " + currentFloor + ": " + loadedPersons);
+        }
+
+
+        /**
+         * @return 3.1 {@link Optional} first (nearest) {@link Floor} in lift travel direction,
+         * where some {@link Person} want to enter OR exit.
+         */
+        private Optional<Floor> firstFloorInTravelDirection() {
+            Optional<Floor> firstFloorPersonWantToEnter = firstFloorInDirectionWherePersonWantToEnter();
+            Optional<Floor> firstFloorPersonWantToExit = firstFloorInDirectionWherePersonWantToExit();
+            return Stream.of(firstFloorPersonWantToEnter, firstFloorPersonWantToExit)
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
+                    .min(Comparator.comparingInt(this::getFloorDistance));
+        }
+
+        /**
+         * @return 3.1 a) {@link Optional} first (nearest) {@link Floor} in lift travel direction,
+         * where some {@link Person} wants to enter.
+         */
+        private Optional<Floor> firstFloorInDirectionWherePersonWantToEnter() {
+            return floorsInTravelDirection().filter(this::hasPersonWantToEnterInDirection).findFirst();
+        }
+
+        private boolean hasPersonWantToEnterInDirection(Floor floor) {
+            if (isGoingUpwards) {
+                return floor.personsGoingUpwards().findAny().isPresent();
+            } else {
+                return floor.personsGoingDownwards().findAny().isPresent();
+            }
+        }
+
+
+        /**
+         * @return 3.1 b) {@link Optional} first (nearest) {@link Floor}, where some {@link #loadedPersons} wants
+         * to exit in lift travel direction
+         */
+        private Optional<Floor> firstFloorInDirectionWherePersonWantToExit() {
+            return floorsInTravelDirection().filter(this::hasLoadedPersonWithDestination).findFirst();
+        }
+
+        private boolean hasLoadedPersonWithDestination(Floor floor) {
+            return loadedPersons.stream()
+                    .map(Person::getDestinationFloor)
+                    .anyMatch(personDesinationFloorLevel -> floor.getFloorLevel() == personDesinationFloorLevel);
+        }
+
+
+        /**
+         * @return 3.2 {@link Optional} last (fares) {@link Floor} in lift travel direction,
+         * where some {@link Person} want to enter in any direction.
+         */
+        private Optional<Floor> lastFloorInDirectionWherePersonWantToEnter() {
+            return floorsInTravelDirection() //
+                    .filter(floor -> floor.personsGoingToOtherFloor().findAny().isPresent()) //
+                    .max(Comparator.comparingInt(this::getFloorDistance)); //
+        }
+
+
+        /**
+         * 4. moves the {@link Lift} to given destinationFloor and add it to {@link #stoppedFloors}
+         *
+         * @param destinationFloor next Floor, where this lift moves to and stops
+         */
+        private void moveLift(Floor destinationFloor) {
+            if (!currentFloor.equals(destinationFloor)) {
+                System.out.println("moveLift from Floor " + currentFloor + " to Floor " + destinationFloor);
+                currentFloor = destinationFloor;
+                stoppedFloors.add(destinationFloor);
+            }
+        }
+
+        private void reverseLiftDirection() {
+            isGoingUpwards = !isGoingUpwards;
+        }
+
+
+        private int getFreePersonCapazity() {
+            return personCapazity - loadedPersons.size();
+        }
+
+        /**
+         * @return A {@link Stream} containing upcoming {@link Floor}s in order for lift travel direction.
+         */
+        private Stream<Floor> floorsInTravelDirection() {
+            if (isGoingUpwards) {
+                return IntStream.range(currentFloor.floorLevel + 1, buildingFloors.length)
+                        .mapToObj(floorLevel -> buildingFloors[floorLevel]);
+            } else {
+                return IntStream.rangeClosed(1, currentFloor.floorLevel)
+                        .mapToObj(floorLevel -> buildingFloors[currentFloor.floorLevel - floorLevel]);
+            }
+        }
+
+        private int getFloorDistance(Floor floor) {
+            return Math.abs(currentFloor.floorLevel - floor.floorLevel);
+        }
+
+    }
+
+
     public static class Floor {
 
         private int floorLevel;
@@ -227,186 +420,6 @@ public class Dinglemouse {
         @Override
         public String toString() {
             return String.valueOf(floorLevel);
-        }
-
-    }
-
-
-    public class Lift {
-
-        private Floor currentFloor;
-
-        private boolean isGoingUpwards = true;
-        private List<Floor> stoppedFloors;
-
-        private int personCapazity;
-        private List<Person> loadedPersons;
-
-
-        public Lift(Floor startingFloor, int personCapazity) {
-            this.currentFloor = startingFloor;
-            this.personCapazity = personCapazity;
-            this.loadedPersons = new LinkedList<>();
-            this.stoppedFloors = new LinkedList<>();
-            this.stoppedFloors.add(startingFloor);
-        }
-
-
-        public int getFreePersonCapazity() {
-            return personCapazity - loadedPersons.size();
-        }
-
-        private boolean isEmpty() {
-            return loadedPersons.isEmpty();
-        }
-
-
-        private void moveLift(Floor destinationFloor) {
-            if (!currentFloor.equals(destinationFloor)) {
-                System.out.println("moveLift from Floor " + currentFloor + " to Floor " + destinationFloor);
-                currentFloor = destinationFloor;
-                stoppedFloors.add(destinationFloor);
-            }
-        }
-
-        private int getFloorDistance(Floor floor) {
-            return Math.abs(currentFloor.floorLevel - floor.floorLevel);
-        }
-
-
-        public void operateLift() {
-            boolean hasMoved = false;
-            System.out.println("                         +-a-+");
-            System.out.println(Dinglemouse.this.toString());
-            for (char step = 'b'; true; step++) {
-                unloadPersons();
-                loadPersons();
-
-                Optional<Floor> nextFloor = firstFloorInTravelDirection();
-                if (!nextFloor.isPresent()) {
-                    nextFloor = latestFloorInDirectionWithPersonsWantToEnter();
-                    reverseLiftDirection();
-                }
-
-                if (nextFloor.isPresent()) {
-                    moveLift(nextFloor.get());
-                    hasMoved = true;
-                } else if (hasMoved) {
-                    hasMoved = false;
-                } else {
-                    moveLift(buildingFloors[0]);
-                    System.out.println("finish with stoppedFloors: " + stoppedFloors);
-                    break;
-                }
-                System.out.println("                         +-" + step + "-+");
-                System.out.println(Dinglemouse.this.toString());
-            }
-        }
-
-        private void reverseLiftDirection() {
-            isGoingUpwards = !isGoingUpwards;
-        }
-
-
-        /**
-         * 1. unloads all persons of the lift, which have as destination {@link #currentFloor}
-         */
-        private void unloadPersons() {
-            for (int i = 0; i < loadedPersons.size(); i++) {
-                Person person = loadedPersons.get(i);
-                if (person.destinationFloor == currentFloor.floorLevel) {
-                    loadedPersons.remove(i--);
-                    currentFloor.personQueue.addLast(person);
-                }
-            }
-            System.out.println("unloadPersons to Floor " + currentFloor + ": " + loadedPersons);
-        }
-
-        /**
-         * 2. load all persons from {@link #currentFloor}, which want to ride in lift direction
-         */
-        private void loadPersons() {
-            List<Person> personsEntering;
-            if (isGoingUpwards) {
-                personsEntering = currentFloor.personsGoingUpwards() //
-                        .limit(getFreePersonCapazity()) //
-                        .collect(Collectors.toList());
-            } else {
-                personsEntering = currentFloor.personsGoingDownwards() //
-                        .limit(getFreePersonCapazity()) //
-                        .collect(Collectors.toList());
-            }
-            personsEntering.forEach(person -> {
-                currentFloor.personQueue.remove(person);
-                loadedPersons.add(person);
-            });
-            System.out.println("loadPersons from Floor " + currentFloor + ": " + loadedPersons);
-        }
-
-
-        /**
-         * @return 3. {@link Optional} first (nearest) {@link Floor}, where some {@link Person} want
-         * to enter OR exit in lift travel direction
-         */
-        private Optional<Floor> firstFloorInTravelDirection() {
-            Optional<Floor> firstFloorPersonWantToEnter = firstFloorInDirectionWherePersonWantToEnter();
-            Optional<Floor> firstFloorPersonWantToExit = firstFloorInDirectionWherePersonWantToExit();
-            return Stream.of(firstFloorPersonWantToEnter, firstFloorPersonWantToExit)
-                    .filter(Optional::isPresent)
-                    .map(Optional::get)
-                    .min(Comparator.comparingInt(this::getFloorDistance));
-        }
-
-        /**
-         * @return 3. a) {@link Optional} first (nearest) {@link Floor}, where some {@link Person} wants to enter in
-         * lift travel direction
-         */
-        private Optional<Floor> firstFloorInDirectionWherePersonWantToEnter() {
-            return floorsInTravelDirection().filter(this::hasPersonsWantToEnterInDirection).findFirst();
-        }
-
-        private boolean hasPersonsWantToEnterInDirection(Floor floor) {
-            if (isGoingUpwards) {
-                return floor.personsGoingUpwards().findAny().isPresent();
-            } else {
-                return floor.personsGoingDownwards().findAny().isPresent();
-            }
-        }
-
-
-        /**
-         * @return 3. b) {@link Optional} first (nearest) {@link Floor}, where some {@link #loadedPersons} wants
-         * to exit in lift travel direction
-         */
-        private Optional<Floor> firstFloorInDirectionWherePersonWantToExit() {
-            return floorsInTravelDirection().filter(this::hasLoadedPersonWithDestination).findFirst();
-        }
-
-        private boolean hasLoadedPersonWithDestination(Floor floor) {
-            return loadedPersons.stream()
-                    .map(Person::getDestinationFloor)
-                    .anyMatch(personDesinationFloorLevel -> floor.getFloorLevel() == personDesinationFloorLevel);
-        }
-
-
-        /**
-         * @return
-         */
-        private Optional<Floor> latestFloorInDirectionWithPersonsWantToEnter() {
-            return floorsInTravelDirection() //
-                    .filter(floor -> floor.personsGoingToOtherFloor().findAny().isPresent()) //
-                    .max(Comparator.comparingInt(this::getFloorDistance)); //
-        }
-
-
-        private Stream<Floor> floorsInTravelDirection() {
-            if (isGoingUpwards) {
-                return IntStream.range(currentFloor.floorLevel + 1, buildingFloors.length)
-                        .mapToObj(floorLevel -> buildingFloors[floorLevel]);
-            } else {
-                return IntStream.rangeClosed(1, currentFloor.floorLevel)
-                        .mapToObj(floorLevel -> buildingFloors[currentFloor.floorLevel - floorLevel]);
-            }
         }
 
     }
