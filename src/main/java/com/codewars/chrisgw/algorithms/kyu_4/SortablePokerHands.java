8f174533db1d1c608699afb6c1ddaaca900c3e30
Chris-GW <Chris-GW@users.noreply.github.com>
2018-09-14 14:07:47 +0200

    start 4 kyu sortable-poker-hands

---
+++ src/main/java/com/codewars/chrisgw/algorithms/kyu_4/SortablePokerHands.java
@@ -0,0 +1,126 @@
+package com.codewars.chrisgw.algorithms.kyu_4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+
+/**
+ * A famous casino is suddenly faced with a sharp decline of their revenues. They decide to offer Texas hold'em also online. Can you help them by writing an algorithm that can rank poker hands?
+ *
+ *
+ * <b style='font-size:16px'>Task:</b>
+ *
+ * <ul>
+ *
+ * <li>Create a poker hand that has a constructor that accepts a string containing 5 cards:</li>
+ *
+ * <li>Apply the <a href="https://en.wikipedia.org/wiki/Texas_hold_%27em">Texas Hold'em</a> rules for ranking the cards. </li>
+ * <li>There is no ranking for the suits.</li>
+ * <li>An ace can either rank high or rank low in a straight or straight flush. Example of a straight with a low ace:<br>
+ * `"5C 4D 3C 2S AS"`</li>
+ * </ul>
+ * </li>
+ *
+ *
+ * </ul>
+ * <br>
+ */
+public class SortablePokerHands {
+
+
+}
+
+
+class PokerHand implements Comparable<PokerHand> {
+
+    public static final int INITIAL_CAPACITY = 5;
+    private List<PokerCard> cards = new ArrayList<>(INITIAL_CAPACITY);
+    private CardSuit cardSuit;
+
+
+    public PokerHand(String hand) {
+        for (String cardStr : hand.split("\\s+")) {
+            char cardValueChar = hand.charAt(0);
+            char cardSuiteChar = hand.charAt(1);
+            this.cardValue = getCardValue(cardValueChar);
+            this.cardSuit = getCardSuite(cardSuiteChar);
+        }
+    }
+
+    private int getCardValue(char cardValueChar) {
+        switch (cardValueChar) {
+        case 'T': /* T(en) */
+            return 10;
+        case 'J': /* J(ack) */
+            return 11;
+        case 'Q': /* Q(ueen) */
+            return 12;
+        case 'K': /* K(ing) */
+            return 13;
+        case 'A': /* A(ce) */
+            return 14;
+        default:
+            return cardValueChar - '0';
+        }
+    }
+
+    private CardSuit getCardSuite(char cardSuiteChar) {
+        return Arrays.stream(CardSuit.values())
+                .filter(cardSuit -> cardSuit.name().charAt(0) == cardSuiteChar)
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+
+    @Override
+    public int compareTo(PokerHand o) {
+        return 0;
+    }
+
+}
+
+
+class PokerCard {
+
+    int cardValue;
+    CardSuit cardSuit;
+
+
+    public PokerCard(int cardValue, CardSuit cardSuit) {
+        this.cardValue = cardValue;
+        this.cardSuit = cardSuit;
+    }
+
+}
+
+
+enum TexasHoldemHand {
+
+    ROYAL_FLUSH(isRoalyFlushPokerHand());
+
+    private static Function<PokerHand, Boolean> isRoalyFlushPokerHand() {
+        return pokerHand -> {
+
+        };
+    }
+
+    HIGHCARD();
+
+
+    private Function<PokerHand, Boolean> isPokerHand;
+
+
+    TexasHoldemHand(Function<PokerHand, Boolean> isPokerHand) {
+        this.isPokerHand = isPokerHand;
+    }
+
+}
+
+
+enum CardSuit {
+
+    SPADES, HEARTS, DIAMONDS, CLUBS; /* in cardSuit value order */
+
+}
---
+++ src/test/java/com/codewars/chrisgw/algorithms/kyu_4/SortablePokerHandsTest.java
@@ -0,0 +1,58 @@
+package com.codewars.chrisgw.algorithms.kyu_4;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class SortablePokerHandsTest {
+
+    @Test
+    public void pokerHandSortTest() {
+        // Arrange
+        ArrayList<PokerHand> expected = new ArrayList<>();
+        expected.add(new PokerHand("KS AS TS QS JS"));
+        expected.add(new PokerHand("2H 3H 4H 5H 6H"));
+        expected.add(new PokerHand("AS AD AC AH JD"));
+        expected.add(new PokerHand("JS JD JC JH 3D"));
+        expected.add(new PokerHand("2S AH 2H AS AC"));
+        expected.add(new PokerHand("AS 3S 4S 8S 2S"));
+        expected.add(new PokerHand("2H 3H 5H 6H 7H"));
+        expected.add(new PokerHand("2S 3H 4H 5S 6C"));
+        expected.add(new PokerHand("2D AC 3H 4H 5S"));
+        expected.add(new PokerHand("AH AC 5H 6H AS"));
+        expected.add(new PokerHand("2S 2H 4H 5S 4C"));
+        expected.add(new PokerHand("AH AC 5H 6H 7S"));
+        expected.add(new PokerHand("AH AC 4H 6H 7S"));
+        expected.add(new PokerHand("2S AH 4H 5S KC"));
+        expected.add(new PokerHand("2S 3H 6H 7S 9C"));
+
+        Random random = new Random();
+        ArrayList<PokerHand> actual = createRandomOrderedList(random, expected);
+
+        // Act
+        Collections.sort(actual);
+
+        // Assert
+        Iterator a = actual.iterator();
+        for (PokerHand e : expected) {
+            assertEquals(e, a.next());
+        }
+    }
+
+    private ArrayList<PokerHand> createRandomOrderedList(Random random, ArrayList<PokerHand> expected) {
+        ArrayList<PokerHand> actual = new ArrayList<>();
+        for (PokerHand pokerHand : expected) {
+            int j = random.nextInt(actual.size() + 1);
+            actual.add(j, pokerHand);
+        }
+        return actual;
+    }
+}
+
+}
